86f892ab5a4a326bc6b3b861a5fa543558ee62cf
Miles-wd7 <mbwolcottdurante@usf.edu>
2025-11-23 20:21:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # app.py
-# Simple Study Time Tracker Program
+# Simple Gaming Time Tracker Program
 # This program asks the user for daily gaming hours, performs a calculation,
 # and includes basic error handling.
 
